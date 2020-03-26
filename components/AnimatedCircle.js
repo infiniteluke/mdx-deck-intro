3070c7100ca68afeec3545befce1bc21a9570548
Luke Herrington <luke.herrington@appfolio.com>
2020-03-26 08:15:28 -0700

    💅 style fixes

--- components/AnimatedCircle.js
+++ components/AnimatedCircle.js
@@ -8,5 +8,5 @@ const AnimatedCircle = animated(Circle);
 export default ({ color, r, x = ['50%', '75%'], y = ['50%', '75%'] }) => {
   const step = useSteps(x.length - 1);
   const props = useSpring({ x: x[step], y: y[step] });
-  return <AnimatedCircle x={props.x} y={props.y} r="50" color={color} r={r} />;
+  return <AnimatedCircle x={props.x} y={props.y} color={color} r={r} />;
 };
